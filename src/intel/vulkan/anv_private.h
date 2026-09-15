joshuaswarren/mesa-1

anv: change bindless debug env variable to be drirc option
This can be toggled now via anv_always_bindless drirc key.

Cc: mesa-stable
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44193>

src/intel/vulkan/anv_private.h
@@ -1825,21 +1825,20 @@ anv_physical_device_has_vram(const struct anv_physical_device *device)
 }
 
 enum anv_debug {
-   ANV_DEBUG_BINDLESS                   = BITFIELD_BIT(0),
-   ANV_DEBUG_NO_GPL                     = BITFIELD_BIT(1),
-   ANV_DEBUG_NO_SECONDARY_CALL          = BITFIELD_BIT(2),
-   ANV_DEBUG_NO_SPARSE                  = BITFIELD_BIT(3),
-   ANV_DEBUG_SPARSE_TRTT                = BITFIELD_BIT(4),
-   ANV_DEBUG_VIDEO_DECODE               = BITFIELD_BIT(5),
-   ANV_DEBUG_VIDEO_ENCODE               = BITFIELD_BIT(6),
-   ANV_DEBUG_SHADER_HASH                = BITFIELD_BIT(7),
-   ANV_DEBUG_NO_SLAB                    = BITFIELD_BIT(8),
-   ANV_DEBUG_DESCRIPTOR_DIRTY           = BITFIELD_BIT(9),
-   ANV_DEBUG_SHADER_PRINT               = BITFIELD_BIT(10),
-   ANV_DEBUG_SHADER_DUMP                = BITFIELD_BIT(11),
-   ANV_DEBUG_EXPERIMENTAL               = BITFIELD_BIT(12),
-   ANV_DEBUG_DGC_DUMP                   = BITFIELD_BIT(13),
-   ANV_DEBUG_SKIP_DISK_CACHE            = BITFIELD_BIT(14),
+   ANV_DEBUG_NO_GPL                     = BITFIELD_BIT(0),
+   ANV_DEBUG_NO_SECONDARY_CALL          = BITFIELD_BIT(1),
+   ANV_DEBUG_NO_SPARSE                  = BITFIELD_BIT(2),
+   ANV_DEBUG_SPARSE_TRTT                = BITFIELD_BIT(3),
+   ANV_DEBUG_VIDEO_DECODE               = BITFIELD_BIT(4),
+   ANV_DEBUG_VIDEO_ENCODE               = BITFIELD_BIT(5),
+   ANV_DEBUG_SHADER_HASH                = BITFIELD_BIT(6),
+   ANV_DEBUG_NO_SLAB                    = BITFIELD_BIT(7),
+   ANV_DEBUG_DESCRIPTOR_DIRTY           = BITFIELD_BIT(8),
+   ANV_DEBUG_SHADER_PRINT               = BITFIELD_BIT(9),
+   ANV_DEBUG_SHADER_DUMP                = BITFIELD_BIT(10),
+   ANV_DEBUG_EXPERIMENTAL               = BITFIELD_BIT(11),
+   ANV_DEBUG_DGC_DUMP                   = BITFIELD_BIT(12),
+   ANV_DEBUG_SKIP_DISK_CACHE            = BITFIELD_BIT(13),
 };
 
 extern enum anv_debug anv_debug;

external-mirrors/mesa

(cherry picked from commit 474e738492fd973ed2ffb6d64f4f62bd62eeb95a)

src/intel/vulkan/anv_private.h
@@ -1818,22 +1818,21 @@ anv_physical_device_has_vram(const struct anv_physical_device *device)
 }
 
 enum anv_debug {
-   ANV_DEBUG_BINDLESS                   = BITFIELD_BIT(0),
-   ANV_DEBUG_NO_GPL                     = BITFIELD_BIT(1),
-   ANV_DEBUG_NO_SECONDARY_CALL          = BITFIELD_BIT(2),
-   ANV_DEBUG_NO_SPARSE                  = BITFIELD_BIT(3),
-   ANV_DEBUG_SPARSE_TRTT                = BITFIELD_BIT(4),
-   ANV_DEBUG_VIDEO_DECODE               = BITFIELD_BIT(5),
-   ANV_DEBUG_VIDEO_ENCODE               = BITFIELD_BIT(6),
-   ANV_DEBUG_SHADER_HASH                = BITFIELD_BIT(7),
-   ANV_DEBUG_NO_SLAB                    = BITFIELD_BIT(8),
-   ANV_DEBUG_DESCRIPTOR_DIRTY           = BITFIELD_BIT(9),
-   ANV_DEBUG_SHADER_PRINT               = BITFIELD_BIT(10),
-   ANV_DEBUG_SHADER_DUMP                = BITFIELD_BIT(11),
-   ANV_DEBUG_EXPERIMENTAL               = BITFIELD_BIT(12),
-   ANV_DEBUG_DGC_DUMP                   = BITFIELD_BIT(13),
-   ANV_DEBUG_NO_ALLOC_OVER_SUBSCRIPTION = BITFIELD_BIT(14),
-   ANV_DEBUG_SKIP_DISK_CACHE            = BITFIELD_BIT(15),
+   ANV_DEBUG_NO_GPL                     = BITFIELD_BIT(0),
+   ANV_DEBUG_NO_SECONDARY_CALL          = BITFIELD_BIT(1),
+   ANV_DEBUG_NO_SPARSE                  = BITFIELD_BIT(2),
+   ANV_DEBUG_SPARSE_TRTT                = BITFIELD_BIT(3),
+   ANV_DEBUG_VIDEO_DECODE               = BITFIELD_BIT(4),
+   ANV_DEBUG_VIDEO_ENCODE               = BITFIELD_BIT(5),
+   ANV_DEBUG_SHADER_HASH                = BITFIELD_BIT(6),
+   ANV_DEBUG_NO_SLAB                    = BITFIELD_BIT(7),
+   ANV_DEBUG_DESCRIPTOR_DIRTY           = BITFIELD_BIT(8),
+   ANV_DEBUG_SHADER_PRINT               = BITFIELD_BIT(9),
+   ANV_DEBUG_SHADER_DUMP                = BITFIELD_BIT(10),
+   ANV_DEBUG_EXPERIMENTAL               = BITFIELD_BIT(11),
+   ANV_DEBUG_DGC_DUMP                   = BITFIELD_BIT(12),
+   ANV_DEBUG_NO_ALLOC_OVER_SUBSCRIPTION = BITFIELD_BIT(13),
+   ANV_DEBUG_SKIP_DISK_CACHE            = BITFIELD_BIT(14),
 };
 
 extern enum anv_debug anv_debug;