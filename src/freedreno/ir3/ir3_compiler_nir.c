external-mirrors/mesa

commit c503b0a1302e51324f10c3250cd04eff3bafe3aa

freedreno/ir3: Implement QCTDD13523866 quirk
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44556>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -5793,6 +5793,45 @@ ir3_remove_noop_subreg_moves(struct ir3 *ir)
    return progress;
 }
 
+static void
+apply_QCTDD13523866(struct ir3_context *ctx)
+{
+   struct ir3 *ir = ctx->ir;
+   struct ir3_instruction *alu = NULL;
+
+   foreach_block (block, &ir->block_list) {
+      if (block->in_early_preamble)
+         continue;
+
+      foreach_instr (instr, &block->instr_list) {
+         if (is_mov(instr) || !is_alu(instr))
+            continue;
+         if (alu)
+            return;
+         alu = instr;
+      }
+   }
+
+   if (!alu)
+      return;
+
+   /* Only a single ALU instruction.. if it has a const src, we must
+    * apply the workaround:
+    */
+   foreach_src (reg, alu) {
+      if (reg->flags & IR3_REG_CONST) {
+         struct ir3_instruction *end = ir3_find_end(ir);
+         struct ir3_instruction *dummy =
+            ir3_build_instr(&ctx->build, OPC_MOV, 1, 1);
+         dummy->cat1.src_type = dummy->cat1.dst_type = TYPE_F32;
+         ir3_src_create(dummy, 0, 0);
+         ir3_dst_create(dummy, 0, 0);
+         ir3_instr_move_before(dummy, end);
+         break;
+      }
+   }
+}
+
 int
 ir3_compile_shader_nir(struct ir3_compiler *compiler,
                        struct ir3_shader *shader,
@@ -6202,6 +6241,9 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
 
    if (ctx->compiler->gen == 4 && ctx->s->info.uses_texture_gather)
       fixup_tg4(ctx);
+
+   if (IR3_QUIRK(ctx->compiler, QCTDD13523866_dummy_alu))
+      apply_QCTDD13523866(ctx);
 
    /* We need to do legalize after (for frag shader's) the "bary.f"
     * offsets (inloc) have been assigned.

src/freedreno/common/freedreno_dev_info.h
@@ -516,6 +516,10 @@ struct fd_dev_info {
       bool QCTDD08407086_cs_lock_unlock : 1;
       /* Whether r8g8 UBWC fast-clear work correctly. */
       bool QCTDD12766770_r8g8_fc_alignment : 1;
+      /* When there is a main shader with single ALU instruction
+       * which has CONST access, insert dummy ALU.
+       */
+      bool QCTDD13523866_dummy_alu : 1;
    } quirks;
 };
 

src/freedreno/common/freedreno_dev_info.py
@@ -39,6 +39,7 @@ class CCUColorCacheFraction(Enum):
 quirk_names = {
     "QCTDD08407086": "cs_lock_unlock",
     "QCTDD12766770": "r8g8_fc_alignment",
+    "QCTDD13523866": "dummy_alu",
 }
 
 class State(object):

src/freedreno/common/freedreno_devices.py
@@ -905,6 +905,7 @@ a7xx_gen3 = GPUProps(
         subpass_fence_cleans_resolve = True,
 
         QCTDD12766770 = True,
+        QCTDD13523866 = True,
     )
 
 a730_magic_regs = dict(

src/freedreno/ir3/ir3.h
@@ -1392,6 +1392,12 @@ is_alu(struct ir3_instruction *instr)
 }
 
 static inline bool
+is_mov(struct ir3_instruction *instr)
+{
+   return opc_cat(instr->opc) == 1;
+}
+
+static inline bool
 is_sfu(struct ir3_instruction *instr)
 {
    return (opc_cat(instr->opc) == 4) || instr->opc == OPC_GETFIBERID;

commit a08a0b93ac53dfb8dff6d2c9cd3893b80aa30f44

ir3: fix QCTDD13523866 workaround
The workaround is supposed to count the number of ALU instructions in
the main shader but erroneously filtered on in_early_preamble. That
didn't work properly either because in_early_preamble is only set after
the workaround is applied. Fix this by only looping ober main shader
blocks.

Totals from 198 (0.11% of 176258) affected shaders:
Instrs: 1583 -> 1555 (-1.77%); split: -7.14%, +5.37%
CodeSize: 12892 -> 12794 (-0.76%); split: -1.63%, +0.87%
MOVs: 597 -> 569 (-4.69%); split: -18.93%, +14.24%
Full: 358 -> 252 (-29.61%); split: -29.89%, +0.28%
(ss): 34 -> 47 (+38.24%); split: -2.94%, +41.18%
Cat1: 597 -> 569 (-4.69%); split: -18.93%, +14.24%

Signed-off-by: Job Noorman <jnoorman@igalia.com>
Fixes: c503b0a1302 ("freedreno/ir3: Implement QCTDD13523866 quirk")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/44719>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -5799,10 +5799,7 @@ apply_QCTDD13523866(struct ir3_context *ctx)
    struct ir3 *ir = ctx->ir;
    struct ir3_instruction *alu = NULL;
 
-   foreach_block (block, &ir->block_list) {
-      if (block->in_early_preamble)
-         continue;
-
+   foreach_main_block (block, ir) {
       foreach_instr (instr, &block->instr_list) {
          if (is_mov(instr) || !is_alu(instr))
             continue;